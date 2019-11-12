b1fa5c2b482e3588d4a2cc6392d06bc205aa6fea
MokRH <55377121+MokRH@users.noreply.github.com>
2019-11-12 12:52:26 +0800

    Update bluetooth.h

--- src/bluetooth.h
+++ src/bluetooth.h
@@ -5,23 +5,41 @@ SoftwareSerial BT(rx, tx); // RX, TX
 //****************** BT Setting ******************//
 
 char get_bt = ' ';
-char BT_receiver() {
+char BT_receiver(int8_t display_getBT) {
   if (BT.available() > 0) {
     get_bt = BT.read();
+
+    if (display_getBT == 0);
+    else if (display_getBT == 0) {
+      oledDisplay(2, 0, 0);
+      display.println("Controller");
+      display.print("signal = ");
+      display.print(get_bt);
+      display.display();
+    }
   }
   return (get_bt);
 }
 
-void rename_bt(String btname) {
+void rename_bt(String btname, int8_t display_name) {
   BT.print("AT+NAME");
   BT.print(btname);
   BT.print("\r\n");
   delay(500);
   BT.println("AT+RESET\r\n");
   delay(500);
+
+  if (display_name == 0);
+  else if (display_name == 1) {
+    oledDisplay(2, 0, 0);
+    display.println("My name is");
+    display.println("----------");
+    display.println(btname);
+    display.display();
+  }
 }
 
-void bt_setup(){
+void bt_setup() {
   //****************** BT setup ******************//
   pinMode(key, OUTPUT); digitalWrite(key, LOW);
   BT.begin(9600);
