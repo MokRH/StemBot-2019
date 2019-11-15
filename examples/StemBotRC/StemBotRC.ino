deaf675ab9a6dd463721e52b2a267f70a078adb1
MokRH <55377121+MokRH@users.noreply.github.com>
2019-11-15 14:18:59 +0800

    Add files via upload

---
+++ examples/StemBotRC/StemBotRC.ino
@@ -0,0 +1,50 @@
+/* OLED screen is deactivate for this combined (RC/auto) program
+   because Arduino is out of memmory
+*/
+//#define OLED //activate this line to use OLED screen
+
+#include "StemBot2019.h"
+#include "bluetooth.h"
+
+/*
+   INSTRUCTIONS Setup
+   bot_setup(IR sensor calibration time in seconds);
+   rename_bt("name as u like", yes/no display on OLED screen);
+   alignment(reduce left speed, reduce right speed, inverted left, inverted right, test);
+   LED(green, yellow, red);
+*/
+void setup() {
+  bot_setup(2);
+  bt_setup();
+  rename_bt("StemBot", 0);
+  alignment(0, 0, 0, 0, 0);
+  LED(1, 0, 1);
+  KP = 0.15, KI = 0, KD = 5;
+}
+
+/*
+   INSTRUCTIONS loop
+   BT_receiver(yes/no display character received from BT);
+   junction(speed, trace back, junction, action, delay b4 turn, turn speed, turn duration, line color 1=black 0=white, IR offset);
+
+   types of junctions
+          robot                type 1         type 2                       type 3                              type 4
+        direction              left           right                        middle                 follow line (dont care any junction)
+
+            H                   HH            HH                HHHHHHHHHHHH        HH
+          HHHHH                 HH            HH                     HH             HH
+        HHHHHHHHH          HHHHHHH            HHHHHHH                HH             HH
+           HHH                  HH            HH                     HH        HHHHHHHHHHHH
+           HHH                  HH            HH                     HH             HH
+           HHH                  HH            HH                     HH             HH
+           HHH                  HH            HH                     HH             HH
+*/
+
+void loop() {
+  if (BT_receiver(0) == 'F') {
+    forward(200, 200);
+  }
+  if (BT_receiver(0) == 'S') {
+    Stop();
+  }
+}
